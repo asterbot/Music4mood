8b4cbfac1b81ab615661355ace4be53b252e852b
asterbot <62688208+asterbot@users.noreply.github.com>
2022-10-08 15:15:39 -0400

    some more stuff

--- emotions.py
+++ emotions.py
@@ -1,5 +1,7 @@
 import text2emotion as te
 
-emotions_value=te.get_emotion("I am happy today")
-print(type(emotions_value))
+s="I got really afraid fear fear fear"
+
+emotions_value=te.get_emotion(s)
+print(emotions_value)
 
--- path.py
+++ path.py
@@ -1,9 +1,10 @@
 import requests
+import text2emotion as te
 
 SPOTIFY_GET_RECOMMENDATIONS_URL = "https://api.spotify.com/v1/recommendations"
-ACCESS_TOKEN = "BQBao0KerTYCi6sH_1FgkBXob85hPY8UcU5aYxxNqAum9di0JYUZ03x-r9CZNUZiBjYws6uD3b-RbUP9JgutAtufKxKAxydEeM7FuWkINbdiioreOE1EFD1F2A8buqqaKAY3_M18LRZu23bS1aA-fLU4nno42tK2nDAu6bermTP4LY_J36qoDiYokDyzmxc"
+ACCESS_TOKEN = "BQAGt89rA_fiyOWgJHCV6RWyP2xUPgjAIidnmZPL_h-aY5tt1qKQlDInYARQgUUYijaeSQ6nNteBxPrR1InPSfejbPDQdt2RN2-rrIv-RomAvKBLj6kEZntmhR6q8oSInnpqzJMnQpUACVyNikhWvZFPOBPEuWTeBMFwUutsoAxb--7jSlBzRESbQYUNQs0"
 
-def get_recommendations_on_spotify(seed_artists,seed_genres,seed_tracks,limit):
+def get_recommendations_on_spotify(seed_artists,seed_genres,seed_tracks,limit,max_tempo,min_tempo,max_valence,min_valence,max_energy,min_energy,max_danceability,min_danceability,max_liveness,min_liveness,max_loudness,min_loudness):
     response=requests.get(
         SPOTIFY_GET_RECOMMENDATIONS_URL,
         headers={
@@ -13,18 +14,47 @@ def get_recommendations_on_spotify(seed_artists,seed_genres,seed_tracks,limit):
             "seed_artists": seed_artists,
             "seed_genres": seed_genres,
             "seed_tracks": seed_tracks,
-            "limit": limit
+            "limit": limit,
+            "max_tempo": max_tempo,
+            "min_tempo": min_tempo,
+            "max_valence": max_valence,
+            "min_valence": min_valence,
+            "max_energy": max_energy,
+            "min_energy": min_energy,
+            "max_danceability": max_danceability,
+            "min_danceability": min_danceability,
+            "max_liveness": max_liveness,
+            "min_liveness": min_liveness,
+            "max_loudness": max_loudness,
+            "min_loudness": min_loudness
         }
     )
     
     return response.json()
 
 def main():
-    artists = "4NHQUGzhtTLFvgF5SZesLK"
-    genres = "classical,country"
+    artists = "4UXqAaa6dQYAk18Lv7PEgX"
+    genres = "pop,country,classical"
     tracks = "0c6xIDDpzE81m2q797ordA"
-    recommendations = get_recommendations_on_spotify(seed_artists=artists,seed_genres=genres,seed_tracks=tracks,limit=3)
-    #print(f"Recommendations: {recommendations}")
+    s=input("Enter your mood: ")
+    #print(te.get_emotion(s))
+    n=int(input("How many songs?"))
+    emotions_value=te.get_emotion(s)
+    max_tempo=min(emotions_value['Fear']+0.5,1)
+    min_tempo=max(emotions_value['Fear']-0.5,0)
+    max_valence=min(emotions_value['Happy']+0.5,1)
+    min_valence=max(emotions_value['Happy']-0.5,0)
+    max_energy=min(max(emotions_value['Happy'],1-emotions_value['Sad'])+0.5,1)
+    min_energy=max(min(emotions_value['Happy'],1-emotions_value['Sad'])-0.5,0)
+    max_danceability=min(1, emotions_value['Happy']+0.5)
+    min_danceability=max(0,emotions_value['Happy']-0.5)
+    max_liveness=min(1, emotions_value['Happy']+0.5)
+    min_liveness=max(0,emotions_value['Happy']-0.5)
+    max_loudness=min(1, (emotions_value['Surprise']+emotions_value['Angry'])/2+0.5)
+    min_loudness=max(0,(emotions_value['Surprise']+emotions_value['Angry'])/2-0.5)
+
+    recommendations = get_recommendations_on_spotify(seed_artists=artists,seed_genres=genres,seed_tracks=tracks,limit=n,max_tempo=max_tempo,min_tempo=min_tempo,max_valence=max_valence,min_valence=min_valence,max_energy=max_energy,min_energy=min_energy,max_danceability=max_danceability,min_danceability=min_danceability,max_liveness=max_liveness,min_liveness=min_liveness,max_loudness=max_loudness,min_loudness=min_loudness)
+    print(f"Recommendations: {recommendations}")
     for i in recommendations['tracks']:
         print(i['name'],"by",i['artists'][0]['name'])
 
