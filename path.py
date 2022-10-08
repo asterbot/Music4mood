a21a67353945a1e063d32d0a19e88c665c36c358
asterbot <62688208+asterbot@users.noreply.github.com>
2022-10-08 14:37:31 -0400

    i fixed it lezgoo

--- path.py
+++ path.py
@@ -1,40 +1,31 @@
 import requests
 
-
-SPOTIFY_GET_RECOMMENDATIONS_URL = f'https://api.spotify.com/v1/recommendations'
-ACCESS_TOKEN = 'BQAcAJui0zV580Fp6FREPh3MrYQbAtCQsTfEhYux-pTBA0GqPe0x99ZJNFscmjexEh0km2JMfNBVvEB2u-V39iVEC97HCZMhwMEBxupZYmc7HHzrPAhvGjX7dzWhxC3mzmsD3b5G91tzBom9p_xgV8e-ffPZv6bC1czEW4PBRI9DqrQ_-RKabZ6_sHYTKpY'
+SPOTIFY_GET_RECOMMENDATIONS_URL = "https://api.spotify.com/v1/recommendations"
+ACCESS_TOKEN = "BQBao0KerTYCi6sH_1FgkBXob85hPY8UcU5aYxxNqAum9di0JYUZ03x-r9CZNUZiBjYws6uD3b-RbUP9JgutAtufKxKAxydEeM7FuWkINbdiioreOE1EFD1F2A8buqqaKAY3_M18LRZu23bS1aA-fLU4nno42tK2nDAu6bermTP4LY_J36qoDiYokDyzmxc"
 
 def get_recommendations_on_spotify(seed_artists,seed_genres,seed_tracks):
     response=requests.get(
         SPOTIFY_GET_RECOMMENDATIONS_URL,
         headers={
-            "Authorization": f"Bearer {ACCESS_TOKEN}",
-            "Content-Type": "application/json",
-            "Host": "api.spotify.com"
+            "Authorization": f"Bearer {ACCESS_TOKEN}"
         },
-        json={
+        params={
             "seed_artists": seed_artists,
             "seed_genres": seed_genres,
             "seed_tracks": seed_tracks
         }
     )
     
-    if (
-    response.status_code != 204 and
-    response.headers["content-type"].strip().startswith("application/json")):
-        try:
-            return response.json()
-        except ValueError:
-            # decide how to handle a server that's misbehaving to this extent
-            pass
-
+    return response.json()
 
 def main():
-    artists = '4UXqAaa6dQYAk18Lv7PEgX'
-    genres = 'pop,classical'
-    tracks = '022DrG7Wp2PSCwzuD0bSzT'
+    artists = "4NHQUGzhtTLFvgF5SZesLK"
+    genres = "classical,country"
+    tracks = "0c6xIDDpzE81m2q797ordA"
     recommendations = get_recommendations_on_spotify(seed_artists=artists,seed_genres=genres,seed_tracks=tracks)
-    print(f"Recommendations: {recommendations}")
+    #print(f"Recommendations: {recommendations}")
+    for i in recommendations['tracks']:
+        print(i['name'],"by",i['artists'][0]['name'])
 
 if __name__ == '__main__':
     main()
