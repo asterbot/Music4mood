c6a99788dd7de997e901e5f84a68a31ae383c8af
Cindy Li <rin.mp4@gmail.com>
2022-10-08 15:25:30 -0400

    Update path.py

--- path.py
+++ path.py
@@ -4,7 +4,7 @@ import text2emotion as te
 SPOTIFY_GET_RECOMMENDATIONS_URL = "https://api.spotify.com/v1/recommendations"
 ACCESS_TOKEN = "BQAGt89rA_fiyOWgJHCV6RWyP2xUPgjAIidnmZPL_h-aY5tt1qKQlDInYARQgUUYijaeSQ6nNteBxPrR1InPSfejbPDQdt2RN2-rrIv-RomAvKBLj6kEZntmhR6q8oSInnpqzJMnQpUACVyNikhWvZFPOBPEuWTeBMFwUutsoAxb--7jSlBzRESbQYUNQs0"
 
-def get_recommendations_on_spotify(seed_artists,seed_genres,seed_tracks,limit,max_tempo,min_tempo,max_valence,min_valence,max_energy,min_energy,max_danceability,min_danceability,max_liveness,min_liveness,max_loudness,min_loudness):
+def get_recommendations_on_spotify(seed_artists,seed_genres,seed_tracks,limit,target_valence,target_energy,target_danceability,target_liveness,target_loudness):
     response=requests.get(
         SPOTIFY_GET_RECOMMENDATIONS_URL,
         headers={
@@ -15,18 +15,11 @@ def get_recommendations_on_spotify(seed_artists,seed_genres,seed_tracks,limit,ma
             "seed_genres": seed_genres,
             "seed_tracks": seed_tracks,
             "limit": limit,
-            "max_tempo": max_tempo,
-            "min_tempo": min_tempo,
-            "max_valence": max_valence,
-            "min_valence": min_valence,
-            "max_energy": max_energy,
-            "min_energy": min_energy,
-            "max_danceability": max_danceability,
-            "min_danceability": min_danceability,
-            "max_liveness": max_liveness,
-            "min_liveness": min_liveness,
-            "max_loudness": max_loudness,
-            "min_loudness": min_loudness
+            "target_valence": target_valence,
+            "target_energy": target_energy,
+            "target_danceability": target_danceability,
+            "target_liveness": target_liveness,
+            "target_loudness": target_loudness
         }
     )
     
@@ -40,21 +33,14 @@ def main():
     #print(te.get_emotion(s))
     n=int(input("How many songs?"))
     emotions_value=te.get_emotion(s)
-    max_tempo=min(emotions_value['Fear']+0.2,1)*2000
-    min_tempo=max(emotions_value['Fear']-0.2,0)*2000
-    max_valence=min(emotions_value['Happy']+0.2,1)
-    min_valence=max(emotions_value['Happy']-0.2,0)
-    max_energy=min(max(emotions_value['Happy'],1-emotions_value['Sad'])+0.2,1)
-    min_energy=max(min(emotions_value['Happy'],1-emotions_value['Sad'])-0.2,0)
-    max_danceability=min(1, emotions_value['Happy']+0.2)
-    min_danceability=max(0,emotions_value['Happy']-0.2)
-    max_liveness=min(1, emotions_value['Happy']+0.2)
-    min_liveness=max(0,emotions_value['Happy']-0.2)
-    max_loudness=min(1, (emotions_value['Surprise']+emotions_value['Angry'])/2+0.2)
-    min_loudness=max(0,(emotions_value['Surprise']+emotions_value['Angry'])/2-0.2)
+    target_valence=(emotions_value['Happy']+1-emotions_value['Happy'])/2
+    target_energy=1-emotions_value['Sad']
+    target_danceability=emotions_value['Happy']
+    target_liveness=emotions_value['Happy']
+    target_loudness=(emotions_value['Angry']+emotions_value['Fear'])/2
 
-    recommendations = get_recommendations_on_spotify(seed_artists=artists,seed_genres=genres,seed_tracks=tracks,limit=n,max_tempo=max_tempo,min_tempo=min_tempo,max_valence=max_valence,min_valence=min_valence,max_energy=max_energy,min_energy=min_energy,max_danceability=max_danceability,min_danceability=min_danceability,max_liveness=max_liveness,min_liveness=min_liveness,max_loudness=max_loudness,min_loudness=min_loudness)
-    print(f"Recommendations: {recommendations}")
+    recommendations = get_recommendations_on_spotify(seed_artists=artists,seed_genres=genres,seed_tracks=tracks,limit=n,target_valence=target_valence,target_energy=target_energy,target_danceability=target_danceability,target_liveness=target_liveness,target_loudness=target_loudness)
+    #print(f"Recommendations: {recommendations}")
     for i in recommendations['tracks']:
         print(i['name'],"by",i['artists'][0]['name'])
 
