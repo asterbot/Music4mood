3ff8c71148cb0da74e3d398d5e45e7d7501b1a56
asterbot <62688208+asterbot@users.noreply.github.com>
2022-10-09 00:47:28 -0400

    ok

---
+++ main.py
@@ -0,0 +1,68 @@
+import requests
+import text2emotion as te
+from flask import Flask, render_template, request
+
+global count
+count=0
+SPOTIFY_GET_RECOMMENDATIONS_URL = "https://api.spotify.com/v1/recommendations"
+ACCESS_TOKEN = "BQATZzvVTd2szHJE6vG0GP7PchyjkcNkomF0fKChJOXcoD8uogdSfqOjsiGlE86jN3_weCRGFSq30zuedacbEPDbRJbX0F-FLlkZRJKrCCRRhWr3KeCMIAA_lcVvKvcGqTQmI7Ca4MYK2XWT06H7N-J4dX9CF39wjpDG3rB2loecINjUx_-mBvgcYa1_DWU"
+main = Flask(__name__, static_folder='static')
+
+@main.route('/', methods=['POST','GET'])
+def index():
+    global count
+    #print("count: ",count)
+         
+    def get_recommendations_on_spotify(seed_artists,seed_genres,seed_tracks,limit,target_valence,target_energy,target_danceability,target_liveness,target_loudness):
+            response=requests.get(
+                SPOTIFY_GET_RECOMMENDATIONS_URL,
+                headers={
+                    "Authorization": f"Bearer {ACCESS_TOKEN}"
+                },
+                params={
+                    "seed_artists": seed_artists,
+                    "seed_genres": seed_genres,
+                    "seed_tracks": seed_tracks,
+                    "limit": limit,
+                    "target_valence": target_valence,
+                    "target_energy": target_energy,
+                    "target_danceability": target_danceability,
+                    "target_liveness": target_liveness,
+                    "target_loudness": target_loudness
+                }
+            )
+            return response.json()
+
+    #print(request.method)
+    if count>=1:
+        text = str(request.values.get("thetext")) #replace 'thetext' with html name tag of input box
+        num = int(request.values.get("thenumoftracks")) #replace 'thenumoftracks' with html name tag of input box
+        print(text)
+        print(num)
+        if request.method == 'GET':
+            #print("ok")
+            artists = "4UXqAaa6dQYAk18Lv7PEgX"
+            genres = "pop,country,classical"
+            tracks = "0c6xIDDpzE81m2q797ordA"
+            emotions_value=te.get_emotion(text)
+            target_valence=(emotions_value['Happy']+1-emotions_value['Sad'])/2
+            target_energy=1-emotions_value['Sad']
+            target_danceability=emotions_value['Happy']
+            target_liveness=emotions_value['Happy']
+            target_loudness=(emotions_value['Angry']+emotions_value['Fear'])/2
+
+            recommendations = get_recommendations_on_spotify(seed_artists=artists,seed_genres=genres,seed_tracks=tracks,limit=num,target_valence=target_valence,target_energy=target_energy,target_danceability=target_danceability,target_liveness=target_liveness,target_loudness=target_loudness)
+            #print(recommendations)
+            all_recommendations = [(str(i['name'])+" by "+str(i['artists'][0]['name'])) for i in recommendations['tracks']]
+            print(all_recommendations)
+            
+            return render_template('index.html', data=all_recommendations)
+
+    count+=1
+    return render_template('index.html',data="nothing to see here")
+
+
+
+
+if __name__ == '__main__':
+    main.run()
